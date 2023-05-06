b57dae9634425b671cfd077cdee0cccb1863cb31
Cody Ogden <cody@codyogden.com>
2023-05-05 21:51:22 -0700

    fix(next.config): Adjust rewrites.

--- apps/source/next.config.js
+++ apps/source/next.config.js
@@ -3,6 +3,10 @@ module.exports = {
         return {
             beforeFiles: [
                 {
+                    source: '/portal',
+                    destination: `${process.env.PORTAL_URL}/portal`
+                },
+                {
                     source: '/portal/:path*',
                     destination: `${process.env.PORTAL_URL}/portal/:path*`
                 },
