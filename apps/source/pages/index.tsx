c91b3354cd8b276fcd8930d8212cab029b610a4e
Cody Ogden <cody@codyogden.com>
2023-05-05 21:41:59 -0700

    chore: Update Next to latest.

--- apps/source/pages/index.tsx
+++ apps/source/pages/index.tsx
@@ -6,7 +6,7 @@ interface IndexPageProps {}
 const IndexPage: NextPage<IndexPageProps> = () => {
   return <>
     <p>This page is served from App A.</p>
-    <p>Go to <Link href='/Portal'>Portal Page</Link>.</p>
+    <p>Go to <Link href='/portal'>Portal Page</Link>.</p>
     <a href="/portal/test">Testing</a>
   </>;
 };
